e1d143e9dd903a6d5b0825f89959e71dbe41f73c
yhaddar <youssefhaddar9@gmail.com>
2023-10-31 20:05:09 +0100

    creation du prject et installation du package

---
+++ resources/js/app.jsx
@@ -0,0 +1,7 @@
+import ReactDOM  from 'react-dom/client';
+import { Home } from "./components/home";
+import './scss/index.css';
+
+ReactDOM.createRoot(document.getElementById("app")).render(
+    <Home />
+)
---
+++ resources/js/components/home.jsx
@@ -0,0 +1,5 @@
+export const Home = () => {
+    return (
+        <h1>hello world</h1>
+    )
+}
